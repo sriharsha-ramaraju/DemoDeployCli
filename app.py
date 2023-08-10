4184f44d3189878e8a85e71639e21cb75bb0db56
sriharsha-ramaraju <rsh960sri@gmail.com>
2023-08-10 19:35:09 +0100

    first commit-main branch-azCli

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app= Flask(__name__)
+
+@app.route('/') # / is for home page , if you say /sriharsha then it will navigate to sriharhsa page inside home page
+def home():
+    return "<center><h1> app which i deployed using azure web servcie using azureCLI <h1>" # for home page to dislay the heading (h1) at the center of the page
+if __name__ == '__main__':
+    app.run(debug=True)
