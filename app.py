b7d62e70ef6e906f6d2a01a54a2cfe3bc9add4cb
sriharsha-ramaraju <rsh960sri@gmail.com>
2023-08-13 22:41:07 +0100

    second commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app= Flask(__name__)
 
 @app.route('/') # / is for home page , if you say /sriharsha then it will navigate to sriharhsa page inside home page
 def home():
-    return "<center><h1> app which i deployed using azure web servcie using azureCLI <h1>" # for home page to dislay the heading (h1) at the center of the page
+    return "<center><h1> app which i deployed using azure web servcie using azureCLI-to see <h1>" # for home page to dislay the heading (h1) at the center of the page
 if __name__ == '__main__':
     app.run(debug=True)
