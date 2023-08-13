7eb8295c9ba583f490cc451360d87d3f852e24ad
sriharsha-ramaraju <rsh960sri@gmail.com>
2023-08-14 00:37:28 +0100

    final commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app= Flask(__name__)
 
 @app.route('/') # / is for home page , if you say /sriharsha then it will navigate to sriharhsa page inside home page
 def home():
-    return "<center><h1> app which i deployed using azure web servcie using azureCLI-this is a child_branch <h1>" # for home page to dislay the heading (h1) at the center of the page
+    return "<center><h1> app which i deployed using azure web servcie using azureCLI-deleted m=child branch <h1>" # for home page to dislay the heading (h1) at the center of the page
 if __name__ == '__main__':
     app.run(debug=True)
